b0f1f3276c3651bd5ccdf8c37d32b25666827117
T.Felipe <85629754+Pollogamerz@users.noreply.github.com>
2023-02-04 14:17:19 -0600

    PlayerMovement

---
+++ Alzheim/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private new Rigidbody rigidbody;
+    public float movementSpeed;
+
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        float hor = Input.GetAxisRaw("Horizontal");
+        float ver = Input.GetAxisRaw("Vertical");
+
+        if(hor != 0 || ver != 0)
+        {
+            Vector3 direction = (transform.forward * ver + transform.right * hor).normalized;
+
+            rigidbody.velocity = direction * movementSpeed;
+        }
+    }
+}
