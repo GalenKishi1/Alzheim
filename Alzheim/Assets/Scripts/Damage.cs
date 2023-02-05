479faa642beb304be71ca10e86aa15ab36840d66
GalenKishi1 <116213207+GalenKishi1@users.noreply.github.com>
2023-02-04 23:55:30 -0600

    Revert "Index Advance"
    This reverts commit 17f3dedbfa12705fb5f03073b92c3a79aaea8ac2.

--- Alzheim/Assets/Scripts/Damage.cs
+++ Alzheim/Assets/Scripts/Damage.cs
@@ -2,15 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Damag : MonoBehaviour
+public class Damage : MonoBehaviour
 {
     private void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.CompareTag("Enemy"))
+        if (other.CompareTag("Enemy"))
         {
             other.GetComponent<EnemyHP>().Quitarvida(10);
             Debug.Log("-50");
         }
     }
-    
 }
