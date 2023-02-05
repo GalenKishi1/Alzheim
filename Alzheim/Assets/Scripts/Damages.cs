29fe47addec738fcce016c35c3fa58a01c75fde2
Pollogamerz <tbarajasglez05@gmail.com>
2023-02-05 00:51:09 -0600

    problemadecodigoresuelto

--- Alzheim/Assets/Scripts/Damages.cs
+++ Alzheim/Assets/Scripts/Damages.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Damage : MonoBehaviour
+public class Damages : MonoBehaviour
 {
     private void OnTriggerEnter(Collider other)
     {
