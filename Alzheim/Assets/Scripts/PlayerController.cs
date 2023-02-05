8bbe13b9ddee2f9c3648ed576f2f60f76889494a
GalenKishi1 <116213207+GalenKishi1@users.noreply.github.com>
2023-02-04 18:55:53 -0600

    Advance

--- Alzheim/Assets/Scripts/PlayerController.cs
+++ Alzheim/Assets/Scripts/PlayerController.cs
@@ -2,8 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class PlayerController : MonoBehaviour
 {
+    PlayerClasses playerClases;
     private new Rigidbody rigidbody;
     public float movementSpeed;
 
